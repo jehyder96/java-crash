588e8062298b82a2540539d45d39a147aeca5a9d
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2023-04-30 19:50:29 +0000

    exercise_01 complete

--- src/labs_examples/datatypes_operators/labs/Exercise_01.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_01.java
@@ -1,5 +1,3 @@
-package labs_examples.datatypes_operators.labs;
-
 /**
  * Data Types and Operators Exercise 1: Variable declaration and Initialization
  *
@@ -17,6 +15,14 @@ public class Exercise_01 {
     public static void main(String[] args) {
 
         // write code here
+        int example1 = 12;
+        double example2 = 20.0;
+        float example3 = 30.00f;
+        char example4 = 'A';
+        System.out.println(example1);
+        System.out.println(example2);
+        System.out.println(example3);
+        System.out.println(example4);
 
     }
 }
