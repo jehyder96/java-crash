8dcc477620d80807e6b58d313fb07d092aef61e6
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2023-05-07 18:57:37 +0000

    conditions_loops exercise 2 complete

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,4 +1,4 @@
-package labs_examples.conditions_loops.labs;
+import java.util.Scanner;
 
 /**
  * Conditions and Loops Exercise 2: Days of the week
@@ -16,6 +16,26 @@ public class Exercise_02 {
     public static void main(String[] args) {
     
         // write completed here
-
+        Scanner dayNumber = new Scanner(System.in); // Creating a Scanner object
+        System.out.println("Enter a number between 1 and 7: ");
+        
+        int dayWeek = dayNumber.nextInt(); // reading user input
+        if (dayWeek == 1) {
+            System.out.println("That number is Monday");
+        } else if (dayWeek == 2) {
+            System.out.println("That number is Tuesday");
+        } else if (dayWeek == 3) {
+            System.out.println("That number is Wednesday");
+        } else if (dayWeek == 4) {
+            System.out.println("That number is Thursday");
+        } else if (dayWeek == 5) {
+            System.out.println("That number is Friday");
+        } else if (dayWeek == 6) {
+            System.out.println("That number is Saturday");
+        } else if (dayWeek == 7) {
+            System.out.println("That number is Sunday");
+        } else {
+            System.out.println("That number isn't between 1 and 7.");
+        }
     }
 }
