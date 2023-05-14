3e2a5b8a821d5575db79b1d6085c03d4479712a9
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2023-05-14 23:47:17 +0000

    solved exercise 1 java array

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,6 +1,6 @@
-package labs_examples.arrays.labs;
-
 import java.util.Arrays;
+import java.util.Scanner;  // Import the Scanner class
+
 
 /**
  * Arrays calculator
@@ -13,5 +13,20 @@ import java.util.Arrays;
  */
 
 public class Exercise_01 {
+    public static void main(String[] args) {
+      int[] arrayCalculator = new int[10]; // Creating the array to store the user input
+      
+      scanner myObj = new Scanner(System.in);  // Create a Scanner object
+      System.out.println("Please type in ten numbers.");
+      
+      //String userInput = myObj.nextLine();  // Read user input
+      int sum = 0; //declaring variable to add all user input together
+      for (int i = 0; i < arrayCalculator.length; i++){ 
+        arrayCalculator[i] = myObj.nextInt();
+        sum = sum + arrayCalculator[i]; // adds all user input and stores it into sum
+        System.out.println(arrayCalculator[i]);
+      }
+      System.out.println("Sum is " + sum + " Average is " + sum/arrayCalculator.length);
+    }
 
 }
