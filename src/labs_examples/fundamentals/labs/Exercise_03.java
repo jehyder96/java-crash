72c1aabfef655d3c5f9b4ce031ecdff7038b3c90
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2023-08-09 09:55:32 -0500

    completed fundamentals exercise 3

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -12,12 +12,26 @@
 
 public class Exercise_03 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        String str = "hello!";
+        // please declare an int variable below, and set it to the value of the length of "str"
+        
+        int strVal = 5;
 
-        // write code to print out "Check out my first program!" below
-        System.out.println("Check out my first program!");
+        String str2 = "hello!";
+        // please initialize a boolean variable and test whether str is equal to str2
+        
+        boolean strIsEqual = true;
+
+        // please concatenate str & str2 and set the result to a new String variable below
+        
+        String str3 = str + str2;
+
+        // please demonstrate the use of any other method that is available to us in the String class
+        // for example, replace(), substring(), contains(), indexOf() etc
+        
+        System.out.println(str3);
 
     }
 
